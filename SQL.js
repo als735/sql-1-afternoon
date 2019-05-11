1140041d921bfd3ff0dd6724cc82cb382edb94a4
Abby Smith <smith@Abbys-MacBook-Pro.local>
2019-05-10 20:10:28 -0600

    first commit

---
+++ SQL.js
@@ -0,0 +1,20 @@
+//TABLE PERSON SECTION 
+// -- CREATE TABLE person ( person_id SERIAL, name VARCHAR(200), age INTEGER, height INTEGER, city VARCHAR(200), favorite_color VARCHAR(200) );
+// -- INSERT INTO person (name, age, height, city, favorite_color ) 
+// -- VALUES ( 'Hillary Brown', 26, 64, 'Provo', 'Pink' );
+// -- SELECT * FROM person
+
+// -- SELECT * FROM person ORDER BY height DESC; 
+// -- SELECT * FROM person ORDER BY height ASC; 
+// -- SELECT * FROM person ORDER BY age DESC; 
+// -- SELECT * FROM person WHERE age > 20; 
+// -- SELECT * FROM person WHERE age = 18; 
+// -- SELECT * FROM person WHERE age < 20 or age > 30; 
+// -- SELECT * FROM person WHERE age != 27; 
+// -- SELECT * FROM person WHERE favorite_color != 'red'; 
+// -- SELECT * FROM person WHERE favorite_color != 'Red' AND favorite_color != 'Blue'; 
+// -- SELECT * FROM person WHERE favorite_color = 'Orange' OR favorite_color = 'GREEN'; 
+// -- SELECT * FROM person WHERE favorite_color IN ('Orange', 'Green', 'Blue');
+// -- SELECT * FROM person WHERE favorite_color IN ('Yellow', 'Purple'); 
+
+// TABLE ORDERS SECTION 
